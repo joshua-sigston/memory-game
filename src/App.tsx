27b68162ca47bb63e239c641d09048bdeaeed2dd
Joshua Sigston <joshua.sigston@outlook.com>
2025-05-23 13:58:55 -0700

    fetch request for emoji

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,22 @@ import MemoryCard from "./components/memory-card";
 function App() {
   const [isGameOn, setIsGameOn] = useState(false);
 
-  function startGame(e: React.FormEvent<HTMLButtonElement>) {
+  async function startGame(e: React.FormEvent<HTMLButtonElement>) {
     e.preventDefault();
+
+    try {
+      const resp = await fetch(
+        `https://emojihub.yurace.pro/api/all/category/animals-and-nature`
+      );
+      if (!resp.ok) {
+        throw new Error(`HTTP error! status: ${resp.status}`);
+      }
+
+      const data = await resp.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
     setIsGameOn(true);
   }
 
